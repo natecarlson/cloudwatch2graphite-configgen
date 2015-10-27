c90d5eb5dbd11115bef42074074b1635e41ce370
Nate Carlson <github@natecarlson.com>
2015-10-27 13:08:58 -0500

    Use a config file to specify regions and attributes; move the generation logic to a function so that we can loop over regions

--- gencfg-rds.rb
+++ gencfg-rds.rb
@@ -5,62 +5,64 @@ require 'aws-sdk-resources'
 require 'json'
 require 'jason'
 
-# Temp hack - load credentials from JSON file; this should change to a real config file.
-creds = JSON.load(File.read('creds.json'))
-Aws.config[:credentials] = Aws::Credentials.new(creds['awsCredentials']['accessKeyId'], creds['awsCredentials']['secretAccessKey'])
+# Load credentials from a credential-specific config file
+$creds = JSON.load(File.read('creds.json'))
+Aws.config[:credentials] = Aws::Credentials.new($creds['awsCredentials']['accessKeyId'], $creds['awsCredentials']['secretAccessKey'])
+$awsaccountnumber = $creds['awsCredentials']['accountNumber']
+
+# Load the rest of the configuration from a standard config file
+config = JSON.load(File.read('config.json'))
+
+awsregions = config['regions']
+$outputmetrics = config['outputmetrics']['RDS']
 
 # Use pretty JSON output format for readability..
 Jason.output_format = :pretty
 
-# AWS Configuration
-@awsregion = "us-east-1"
-@awsaccountnumber = creds['awsCredentials']['accountNumber']
+# Output config file for RDS for a given region
+def generateRDSConfig(awsregion)
+  @awsregion = awsregion
+  @rdsinstances = Array.new
 
-# Items to output for each host in the JSON config; check the AWS CloudWatch page to get a list,
-# IE, for RDS, http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/rds-metricscollected.html
-outputmetricnames = ["CPUUtilization", "DatabaseConnections", "DiskQueueDepth", "FreeableMemory", "FreeStorageSpace", "SwapUsage", "ReadIOPS", "WriteIOPS", "ReadLatency", "WriteLatency", "ReadThroughput", "WriteThroughput", "NetworkReceiveThroughput", "NetworkTransmitThroughput"]
+  Aws.config.update({
+	  region: "#{awsregion}",
+  })
 
-Aws.config.update({
-	region: "#@awsregion",
-})
+  rds = Aws::RDS::Client.new(region: awsregion)
+  rds.describe_db_instances.each do |instances|
+	  instances.db_instances.each do |instance|
+		  @instancename = instance.db_instance_identifier
+  		# AWS's SDK is silly, and doesn't let you look up tags based on a RDS DB name. Have to
+	  	# pass in an 'ARN' instead - and the SDK doesn't provide the ARN to us. Have to generate it.
+		  # Really, really lame.
+  		@instancearn = "arn:aws:rds:#{awsregion}:#$awsaccountnumber:db:#@instancename"
+	  	rdsinstance = Hash.new
+		  rdsinstance['name'] = "#@instancename"
+  		rdsinstance['arn']  = "#@instancearn"
+	  	rdsinstance["tags"] = Hash.new
+		  rds.list_tags_for_resource({ resource_name: @instancearn }).each do |tags|
+  			tags.tag_list.each do |tag|
+	  			rdsinstance["tags"]["#{tag.key}"]  = "#{tag.value}"
+		  	end
+  		end
 
-rdsinstances = Array.new
+	  	if rdsinstance["tags"]["env"] == "prod" then
+		  	@rdsinstances.push(rdsinstance)
+  		end
+	  end
+  end
 
-rds = Aws::RDS::Client.new(region: @awsregion)
-rds.describe_db_instances.each do |instances|
-	instances.db_instances.each do |instance|
-		@instancename = instance.db_instance_identifier
-		# AWS's SDK is silly, and doesn't let you look up tags based on a RDS DB name. Have to
-		# pass in an 'ARN' instead - and the SDK doesn't provide the ARN to us. Have to generate it.
-		# Really, really lame.
-		@instancearn = "arn:aws:rds:#@awsregion:#@awsaccountnumber:db:#@instancename"
-		rdsinstance = Hash.new
-		rdsinstance['name'] = "#@instancename"
-		rdsinstance['arn']  = "#@instancearn"
-		rdsinstance["tags"] = Hash.new
-		rds.list_tags_for_resource({ resource_name: @instancearn }).each do |tags|
-			tags.tag_list.each do |tag|
-				rdsinstance["tags"]["#{tag.key}"]  = "#{tag.value}"
-			end
-		end
-
-		if rdsinstance["tags"]["env"] == "prod" then
-			rdsinstances.push(rdsinstance)
-		end
-	end
-end
-
-puts Jason.render(<<-EOS
+  puts Jason.render(<<-EOS
 {
   "awsCredentials": {
-    "accessKeyId": <%= creds['awsCredentials']['accessKeyId'] %>,
-    "secretAccessKey": <%= creds['awsCredentials']['secretAccessKey'] %>,
+    "accessKeyId": <%= $creds['awsCredentials']['accessKeyId'] %>,
+    "secretAccessKey": <%= $creds['awsCredentials']['secretAccessKey'] %>,
     "region": <%= @awsregion %>,
   },  
   "metricsConfig": {
     "metrics": [
-<% rdsinstances.each do |instance| -%>
-  <% outputmetricnames.each do |metricname| -%>
+<% @rdsinstances.each do |instance| -%>
+  <% $outputmetrics.each do |metricname| -%>
       {
         "Namespace": "AWS/RDS",
         "MetricName": <%= metricname %>,
@@ -82,4 +84,10 @@ puts Jason.render(<<-EOS
   }
 }
 EOS
-)
+  )
+end
+
+awsregions.each do |awsregion|
+  generateRDSConfig(awsregion)
+end
+
